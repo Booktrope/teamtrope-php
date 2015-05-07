c784462d496570d7faf2960152f256f026bd448e
Justin Jeffress <justin.jeffress@booktrope.com>
2015-05-06 18:25:19 -0700

    Plugin - export attachment urls from teamtrope
    Plugin - export attachment urls from teamtrope
    
    Signed-off-by: Justin Jeffress <justin.jeffress@booktrope.com>

---
+++ wp-content/plugins/bt_attachment_export/main.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Plugin Name: Booktrope Attachment Export
+Plugin URI:  http://booktrope.com
+Description: Export project attachments to a csv file
+Author: Justin Jeffress
+Version: 0.0.1
+Author URI: http://booktrope.com
+License: GPLv2 or later
+*/
+
+define('BTAE_PATH', plugin_dir_path(__FILE__));
+
+require_once BTAE_PATH.'includes/btae-settings-menu.php'
+
+
+?>
